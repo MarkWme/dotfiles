8ab7258c386bd5f68430a5dc98cc1730f894c720
Mark Whitby <github@chipfat.com>
2020-02-06 21:29:31 +0000

    Added external monitor script

--- .config/polybar/polybar-scripts/expressvpn-status.sh
+++ .config/polybar/polybar-scripts/expressvpn-status.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 STATUS=$(expressvpn status | grep "Connected" | cut -d " " -f1 | tr -d " ")
-if [ "$STATUS"="Connected" ]; then
+if [ $STATUS="Connected" ]; then
 	echo "%{F#AAA}ExpressVPN: $(expressvpn status | grep "Connected" | cut -d " " -f3-)%{F-}"
     else
 	echo ""
--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 ln -s ~/repos/dotfiles/.config/polybar ~/.config/polybar
 ln -s ~/repos/dotfiles/.config/i3 ~/.config/i3
 ln -s ~/repos/dotfiles/.Xresources ~/.Xresources
+ln -s ~/repos/dotfiles/scripts ~/scripts
 
